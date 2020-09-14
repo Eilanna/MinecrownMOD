c57b1dd27ce9a5b100843cf1c5afe3fc12059525
MeOw <meow_hrne@hotmail.fr>
2020-09-14 23:11:41 +0200

    ajout texture bloc grinder

--- src/main/java/net/taki/minecrown/MinecrownMOD.java
+++ src/main/java/net/taki/minecrown/MinecrownMOD.java
@@ -45,7 +45,7 @@ import java.util.function.Supplier;
 @Mod(modid = MinecrownMOD.MODID, version = MinecrownMOD.VERSION)
 public class MinecrownMOD {
 	public static final String MODID = "minecrown";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "0.0.1";
 	public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("minecrown:a");
 	@SidedProxy(clientSide = "net.taki.minecrown.ClientProxyMinecrownMOD", serverSide = "net.taki.minecrown.ServerProxyMinecrownMOD")
 	public static IProxyMinecrownMOD proxy;
