88a469ab0d485966cbd59881da85877bc3a86fc7
MeOw <meow_hrne@hotmail.fr>
2020-09-24 17:37:02 +0200

    Test de Buisson3D réussi + Test de blocs Supports en bois pour Menuisier Réussi

---
+++ src/main/java/net/taki/minecrown/block/BlockJsonBush.java
@@ -0,0 +1,108 @@
+
+package net.taki.minecrown.block;
+
+import net.taki.minecrown.creativetab.TabMinecrownPaysanTable;
+import net.taki.minecrown.ElementsMinecrownMOD;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.gen.feature.WorldGenFlowers;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.World;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.NonNullList;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockFlower;
+import net.minecraft.block.Block;
+
+import java.util.Random;
+
+@ElementsMinecrownMOD.ModElement.Tag
+public class BlockJsonBush extends ElementsMinecrownMOD.ModElement {
+	@GameRegistry.ObjectHolder("minecrown:jsonbush")
+	public static final Block block = null;
+	public BlockJsonBush(ElementsMinecrownMOD instance) {
+		super(instance, 122);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustomFlower());
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("minecrown:jsonbush", "inventory"));
+	}
+
+	@Override
+	public void generateWorld(Random random, int chunkX, int chunkZ, World world, int dimID, IChunkGenerator cg, IChunkProvider cp) {
+		boolean dimensionCriteria = false;
+		if (dimID == 0)
+			dimensionCriteria = true;
+		if (!dimensionCriteria)
+			return;
+		for (int i = 0; i < 20; i++) {
+			int l6 = chunkX + random.nextInt(16) + 8;
+			int i11 = random.nextInt(128);
+			int l14 = chunkZ + random.nextInt(16) + 8;
+			(new WorldGenFlowers(((BlockFlower) block), BlockFlower.EnumFlowerType.DANDELION)).generate(world, random, new BlockPos(l6, i11, l14));
+		}
+	}
+	public static class BlockCustomFlower extends BlockFlower {
+		public BlockCustomFlower() {
+			setSoundType(SoundType.GROUND);
+			setCreativeTab(TabMinecrownPaysanTable.tab);
+			setHardness(0F);
+			setResistance(0F);
+			setLightLevel(0F);
+			setUnlocalizedName("jsonbush");
+			setRegistryName("jsonbush");
+		}
+
+		@Override
+		public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+			drops.add(new ItemStack(this));
+		}
+
+		@Override
+		public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+			return new ItemStack(Item.getItemFromBlock(this), 1, this.damageDropped(state));
+		}
+
+		@Override
+		public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
+			return EnumPlantType.Plains;
+		}
+
+		@Override
+		public BlockFlower.EnumFlowerColor getBlockType() {
+			return BlockFlower.EnumFlowerColor.YELLOW;
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public void getSubBlocks(CreativeTabs tab, net.minecraft.util.NonNullList<ItemStack> list) {
+			for (BlockFlower.EnumFlowerType blockflower$enumflowertype : BlockFlower.EnumFlowerType.getTypes(this.getBlockType())) {
+				list.add(new ItemStack(this, 1, blockflower$enumflowertype.getMeta()));
+			}
+		}
+	}
+}
--- src/main/java/net/taki/minecrown/block/BlockMachineAMoudreblock.java
+++ src/main/java/net/taki/minecrown/block/BlockMachineAMoudreblock.java
@@ -14,10 +14,13 @@ import net.minecraftforge.client.event.ModelRegistryEvent;
 
 import net.minecraft.world.World;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.Mirror;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.tileentity.TileEntityLockableLoot;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
@@ -32,11 +35,15 @@ import net.minecraft.inventory.ContainerChest;
 import net.minecraft.inventory.Container;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.Block;
 
 @ElementsMinecrownMOD.ModElement.Tag
@@ -65,15 +72,64 @@ public class BlockMachineAMoudreblock extends ElementsMinecrownMOD.ModElement {
 				new ModelResourceLocation("minecrown:machineamoudreblock", "inventory"));
 	}
 	public static class BlockCustom extends Block implements ITileEntityProvider {
+		public static final PropertyDirection FACING = BlockHorizontal.FACING;
 		public BlockCustom() {
-			super(Material.IRON);
+			super(Material.ANVIL);
 			setUnlocalizedName("machineamoudreblock");
 			setSoundType(SoundType.METAL);
 			setHardness(1F);
 			setResistance(10F);
 			setLightLevel(0F);
-			setLightOpacity(255);
+			setLightOpacity(0);
 			setCreativeTab(TabMinecrownPaysanTable.tab);
+			this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public BlockRenderLayer getBlockLayer() {
+			return BlockRenderLayer.CUTOUT;
+		}
+
+		@Override
+		protected net.minecraft.block.state.BlockStateContainer createBlockState() {
+			return new net.minecraft.block.state.BlockStateContainer(this, new IProperty[]{FACING});
+		}
+
+		@Override
+		public IBlockState withRotation(IBlockState state, Rotation rot) {
+			return state.withProperty(FACING, rot.rotate((EnumFacing) state.getValue(FACING)));
+		}
+
+		@Override
+		public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+			return state.withRotation(mirrorIn.toRotation((EnumFacing) state.getValue(FACING)));
+		}
+
+		@Override
+		public IBlockState getStateFromMeta(int meta) {
+			return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta));
+		}
+
+		@Override
+		public int getMetaFromState(IBlockState state) {
+			return ((EnumFacing) state.getValue(FACING)).getIndex();
+		}
+
+		@Override
+		public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta,
+				EntityLivingBase placer) {
+			return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+		}
+
+		@Override
+		public boolean isOpaqueCube(IBlockState state) {
+			return false;
+		}
+
+		@Override
+		public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+			return false;
 		}
 
 		@Override
