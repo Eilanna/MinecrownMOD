742e10d800d9c2fc2410e8e22fc9d214bfaae0f7
MeOw <meow_hrne@hotmail.fr>
2020-09-26 18:24:14 +0200

    Ajout plantation d'Orge (Objets et Items) Manque Procédures

---
+++ src/main/java/net/taki/minecrown/block/BlockCropBarleyPlant3.java
@@ -0,0 +1,238 @@
+
+package net.taki.minecrown.block;
+
+import net.taki.minecrown.item.ItemCropBarleySeed;
+import net.taki.minecrown.item.ItemCropBarleyItem;
+import net.taki.minecrown.ElementsMinecrownMOD;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.tileentity.TileEntityLockableLoot;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.inventory.ContainerChest;
+import net.minecraft.inventory.Container;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.Block;
+
+@ElementsMinecrownMOD.ModElement.Tag
+public class BlockCropBarleyPlant3 extends ElementsMinecrownMOD.ModElement {
+	@GameRegistry.ObjectHolder("minecrown:cropbarleyplant3")
+	public static final Block block = null;
+	public BlockCropBarleyPlant3(ElementsMinecrownMOD instance) {
+		super(instance, 170);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("cropbarleyplant3"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		GameRegistry.registerTileEntity(TileEntityCustom.class, "minecrown:tileentitycropbarleyplant3");
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("minecrown:cropbarleyplant3", "inventory"));
+	}
+	public static class BlockCustom extends Block implements ITileEntityProvider {
+		public BlockCustom() {
+			super(Material.FIRE);
+			setUnlocalizedName("cropbarleyplant3");
+			setSoundType(SoundType.PLANT);
+			setHarvestLevel("axe", 0);
+			setHardness(0F);
+			setResistance(0F);
+			setLightLevel(0F);
+			setLightOpacity(0);
+			setCreativeTab(null);
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public BlockRenderLayer getBlockLayer() {
+			return BlockRenderLayer.CUTOUT_MIPPED;
+		}
+
+		@Override
+		@javax.annotation.Nullable
+		public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+			return NULL_AABB;
+		}
+
+		@Override
+		public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public boolean isFullCube(IBlockState state) {
+			return false;
+		}
+
+		@Override
+		public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+			return new AxisAlignedBB(0D, 0.001D, 0D, 1D, 1D, 1D);
+		}
+
+		@Override
+		public int tickRate(World world) {
+			return 100;
+		}
+
+		@Override
+		public boolean isOpaqueCube(IBlockState state) {
+			return false;
+		}
+
+		@Override
+		public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+			return new ItemStack(ItemCropBarleySeed.block, (int) (1));
+		}
+
+		@Override
+		public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+			return false;
+		}
+
+		@Override
+		public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+			drops.add(new ItemStack(ItemCropBarleyItem.block, (int) (0)));
+		}
+
+		@Override
+		public TileEntity createNewTileEntity(World worldIn, int meta) {
+			return new TileEntityCustom();
+		}
+
+		@Override
+		public boolean eventReceived(IBlockState state, World worldIn, BlockPos pos, int eventID, int eventParam) {
+			super.eventReceived(state, worldIn, pos, eventID, eventParam);
+			TileEntity tileentity = worldIn.getTileEntity(pos);
+			return tileentity == null ? false : tileentity.receiveClientEvent(eventID, eventParam);
+		}
+
+		@Override
+		public EnumBlockRenderType getRenderType(IBlockState state) {
+			return EnumBlockRenderType.MODEL;
+		}
+	}
+
+	public static class TileEntityCustom extends TileEntityLockableLoot {
+		private NonNullList<ItemStack> stacks = NonNullList.<ItemStack>withSize(0, ItemStack.EMPTY);
+		@Override
+		public int getSizeInventory() {
+			return 0;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			for (ItemStack itemstack : this.stacks)
+				if (!itemstack.isEmpty())
+					return false;
+			return true;
+		}
+
+		@Override
+		public boolean isItemValidForSlot(int index, ItemStack stack) {
+			return true;
+		}
+
+		@Override
+		public ItemStack getStackInSlot(int slot) {
+			return stacks.get(slot);
+		}
+
+		@Override
+		public String getName() {
+			return "container.cropbarleyplant3";
+		}
+
+		@Override
+		public void readFromNBT(NBTTagCompound compound) {
+			super.readFromNBT(compound);
+			this.stacks = NonNullList.<ItemStack>withSize(this.getSizeInventory(), ItemStack.EMPTY);
+			if (!this.checkLootAndRead(compound))
+				ItemStackHelper.loadAllItems(compound, this.stacks);
+		}
+
+		@Override
+		public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+			super.writeToNBT(compound);
+			if (!this.checkLootAndWrite(compound))
+				ItemStackHelper.saveAllItems(compound, this.stacks);
+			return compound;
+		}
+
+		@Override
+		public SPacketUpdateTileEntity getUpdatePacket() {
+			return new SPacketUpdateTileEntity(this.pos, 0, this.getUpdateTag());
+		}
+
+		@Override
+		public NBTTagCompound getUpdateTag() {
+			return this.writeToNBT(new NBTTagCompound());
+		}
+
+		@Override
+		public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+			this.readFromNBT(pkt.getNbtCompound());
+		}
+
+		@Override
+		public void handleUpdateTag(NBTTagCompound tag) {
+			this.readFromNBT(tag);
+		}
+
+		@Override
+		public int getInventoryStackLimit() {
+			return 64;
+		}
+
+		@Override
+		public String getGuiID() {
+			return "minecrown:cropbarleyplant3";
+		}
+
+		@Override
+		public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn) {
+			this.fillWithLoot(playerIn);
+			return new ContainerChest(playerInventory, this, playerIn);
+		}
+
+		@Override
+		protected NonNullList<ItemStack> getItems() {
+			return this.stacks;
+		}
+	}
+}
---
+++ src/main/java/net/taki/minecrown/item/ItemCropBarleySeed.java
@@ -0,0 +1,60 @@
+
+package net.taki.minecrown.item;
+
+import net.taki.minecrown.creativetab.TabMiCroJobsAgriculteur;
+import net.taki.minecrown.ElementsMinecrownMOD;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+@ElementsMinecrownMOD.ModElement.Tag
+public class ItemCropBarleySeed extends ElementsMinecrownMOD.ModElement {
+	@GameRegistry.ObjectHolder("minecrown:cropbarleyseed")
+	public static final Item block = null;
+	public ItemCropBarleySeed(ElementsMinecrownMOD instance) {
+		super(instance, 165);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("minecrown:cropbarleyseed", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("cropbarleyseed");
+			setRegistryName("cropbarleyseed");
+			setCreativeTab(TabMiCroJobsAgriculteur.tab);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
